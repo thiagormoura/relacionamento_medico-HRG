f5d44b4be3f9d4cf4f413d03ca3a3f8d34f2489c
joaothiago <joaothiagormoura@gmail.com>
2024-06-24 11:35:27 -0300

    verificacao de banco

--- enviarindexbanco.php
+++ enviarindexbanco.php
@@ -5,60 +5,53 @@ $dbname = "relacionamentomedico";
 $dbuser = "root";
 $dbpass = "";
 
-
-function enviarParaBanco($conn, $date, $situacaoatendimento, $nome, $registro, $orgao, $celular, $celulardois, $nascimento, $email, $endereco, $especialidade, $assunto, $descricao, $acoes, $cpf, $tipo_atendimento,$veiculoselecionado) {
+// Função para enviar dados para o banco de dados
+function enviarParaBanco($conn, $date, $status, $nome, $registro, $orgao, $celular, $celulardois, $nascimento, $email, $endereco, $especialidade, $assunto, $descricao, $acoes, $cpf, $tipo_atendimento, $veiculoselecionado, $assuntoatendimento, $assuntosselecionados) {
     $sucesso = true;
 
+    // Inserir dados na tabela profissionais
+    $sql_profissionais = "INSERT INTO profissionais (data_nascimento, cpf, email, telefone, telefone2, nome, registro, especialidades, orgao, endereco) VALUES ('$nascimento', '$cpf', '$email', '$celular', '$celulardois', '$nome', '$registro', '$especialidade', '$orgao', '$endereco')";
+    if ($conn->query($sql_profissionais) !== TRUE) {
+        echo "Erro ao inserir dados na tabela profissionais: " . $conn->error . "<br>";
+        $sucesso = false;
+    } else {
+        // Obter o ID inserido na tabela profissionais
+        $id_profissional = $conn->insert_id;
 
-    
- 
-       // Inserir dados na tabela acoes
-$sql_medicos = "INSERT INTO descricao (descricao) VALUES ('$descricao')";
-if ($conn->query($sql_medicos) !== TRUE) {
-    echo "Erro ao inserir dados do médico na tabela profissionais: " . $conn->error . "<br>";
-    $sucesso = false;
-} else {
-    
-      
-    }
+        // Inserir dados na tabela atendimento
+        $sql_atendimento = "INSERT INTO atendimento (profissional, assunto , acoes , descricao , situacao , veiculo_atendimento, data) VALUES ('$id_profissional', '$assuntoatendimento', '$acoes', '$descricao', '$status', '$veiculoselecionado', '$date')";
+        if ($conn->query($sql_atendimento) !== TRUE) {
+            echo "Erro ao inserir dados na tabela atendimento: " . $conn->error . "<br>";
+            $sucesso = false;
+        } else {
+            // Obter o ID inserido na tabela atendimento
+            $id_atendimento = $conn->insert_id;
 
-   // Inserir dados na tabela acoes
-$sql_medicos = "INSERT INTO acoes (acoes) VALUES ('$acoes')";
-if ($conn->query($sql_medicos) !== TRUE) {
-    echo "Erro ao inserir dados do médico na tabela profissionais: " . $conn->error . "<br>";
-    $sucesso = false;
-} else {
-    
-      
-    }
-   
-
-// Inserir dados na tabela profissionais
-$sql_medicos = "INSERT INTO profissionais (data_nascimento, cpf, email, telefone,telefone2, nome ,registro,especialidades ,orgao, endereco,veiculo_manifestacao) VALUES ('$nascimento', '$cpf', '$email', '$celular','$celulardois', '$nome','$registro','$especialidade','$orgao','$endereco','$veiculoselecionado')";
-if ($conn->query($sql_medicos) !== TRUE) {
-    echo "Erro ao inserir dados do médico na tabela profissionais: " . $conn->error . "<br>";
-    $sucesso = false;
-} else {
-    
-      
+            // Inserir dados na tabela atendimento_has_assunto
+            $sql_atendimento_has_assunto = "INSERT INTO atendimento_has_assunto (atendimento , assunto) VALUES ('$id_atendimento', '$assuntosselecionados')";
+            if ($conn->query($sql_atendimento_has_assunto) !== TRUE) {
+                echo "Erro ao inserir dados na tabela atendimento_has_assunto: " . $conn->error . "<br>";
+                $sucesso = false;
+            }
+        }
     }
 
     return $sucesso;
 }
 
-
+// Conectar ao banco de dados
 $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
-
+// Verificar conexão
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 }
 
-
+// Obter dados do formulário via POST
 $date = isset($_POST['date']) ? $_POST['date'] : null;
 $tipo_atendimento = isset($_POST['tipo_atendimento']) ? $_POST['tipo_atendimento'] : null;
 $celulardois = isset($_POST['celulardois']) ? $_POST['celulardois'] : null;
-$situacaoatendimento = isset($_POST['situacao_atendimento']) ? $_POST['situacao_atendimento'] : null;
+$status = isset($_POST['status']) ? $_POST['status'] : null;
 $nome = isset($_POST['nome']) ? $_POST['nome'] : null;
 $registro = isset($_POST['registro']) ? $_POST['registro'] : null;
 $orgao = isset($_POST['orgao']) ? $_POST['orgao'] : null;
@@ -72,13 +65,16 @@ $descricao = isset($_POST['descricao']) ? $_POST['descricao'] : null;
 $acoes = isset($_POST['acoes']) ? $_POST['acoes'] : null;
 $cpf = isset($_POST['cpf']) ? $_POST['cpf'] : null;
 $veiculoselecionado = isset($_POST['veiculoselecionado']) ? $_POST['veiculoselecionado'] : null;
+$assuntoatendimento = isset($_POST['assuntoatendimento']) ? $_POST['assuntoatendimento'] : null;
+$assuntosselecionados = isset($_POST['assuntosselecionadosString']) ? $_POST['assuntosselecionadosString'] : null;
 
-
-if (enviarParaBanco($conn, $date, $situacaoatendimento, $nome, $registro, $orgao, $celular, $celulardois, $nascimento, $email, $endereco, $especialidade, $assunto, $descricao, $acoes, $cpf, $tipo_atendimento,$veiculoselecionado)) {
+// Enviar dados para o banco de dados
+if (enviarParaBanco($conn, $date, $status, $nome, $registro, $orgao, $celular, $celulardois, $nascimento, $email, $endereco, $especialidade, $assunto, $descricao, $acoes, $cpf, $tipo_atendimento, $veiculoselecionado, $assuntoatendimento, $assuntosselecionados)) {
     echo "Dados enviados com sucesso para o banco de dados.";
 } else {
     echo "Erro ao enviar dados para o banco de dados.";
 }
 
+// Fechar a conexão
 $conn->close();
-
+?>
