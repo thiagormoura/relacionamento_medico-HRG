37625ad0dc1b4535739ac0b49af8d5d8a94ca893
PedroHenriqueCustodiodeLima <ph4261009@gmail.com>
2024-06-27 11:20:11 -0300

    tabela profissionais, edit profissionais

--- profissionais.php
+++ profissionais.php
@@ -142,11 +142,11 @@ $totalPaginas = ceil($totalRegistros / $registrosPorPagina);
     <table class="table table-bordered table-striped">
     <thead class="thead-light">
         <tr>
-            <th class="text-left">Data</th>
-            <th class="text-left">Nome do Profissional</th>
-            <th class="text-left">Assunto Tratado</th>
-            <th class="text-left">Status</th>
-            <th class="text-center">Dados</th>
+            <th class="text-left">CPF</th>
+            <th class="text-left">NOME</th>
+            <th class="text-left">EMAIL</th>
+            <th class="text-left">TELEFONE</th>
+            <th class="text-center">EDITAR</th>
         </tr>
     </thead>
     <tbody id="tableBody">
@@ -180,18 +180,16 @@ $totalPaginas = ceil($totalRegistros / $registrosPorPagina);
             $telefone = htmlspecialchars($row['telefone']);
             $telefone2 = htmlspecialchars($row['telefone2']);
             $email = htmlspecialchars($row['email']);
-            $assunto = htmlspecialchars($row['assunto']) ? htmlspecialchars($row['assunto']) : "Nenhum assunto encontrado";
-            $situacao = htmlspecialchars($row['situacao']);  
             echo "<tr>";
-            echo "<td class='text-left'>" . htmlspecialchars($data_atendimento_formatada) . "</td>";
+            echo "<td class='text-left'>" . htmlspecialchars($cpf) . "</td>";
             echo "<td class='text-left'>" . htmlspecialchars($row['nome']) . "</td>";
-            echo "<td class='text-left'>" . $assunto . "</td>";
-            echo "<td class='text-left'><span class='" . getBadgeClass($situacao) . "'>" . $situacao . "</span></td>";
+            echo "<td class='text-left'>" . $email . "</td>";
+            echo "<td class='text-left'>" . $telefone . "</td>";
             echo "<td class='text-center'>";
             echo "<button class='btn btn-primary' onclick='redirectToDetails({$row['id_atendimento']})' style='background-color: transparent; border: none;'>";
-            echo "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 576 512' width='20' height='20'>";
-            echo "<path fill='#001f3f' d='M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z'/>";
-            echo "</svg>";
+            echo "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512' width='20' height='20' style='cursor: pointer;' onclick='editAtendimento({$row['id']})'>";
+                echo "<path fill='#001f3f' d='M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z'/>";
+                echo "</svg>";
             echo "</button>";
             echo "</td>";
             echo "</tr>";
@@ -207,7 +205,7 @@ $totalPaginas = ceil($totalRegistros / $registrosPorPagina);
 
 <script>
     function redirectToDetails(id) {
-        window.location.href = 'dados.php?id=' + id;
+        window.location.href = 'editar_profissional.php?id=' + id;
     }
 </script>
 
