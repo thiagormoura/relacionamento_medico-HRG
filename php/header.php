4d7dc5a0e9a1fca3a8ac361171414fbd694548b9
bygabrielmendes <ggomesmendes2004@gmail.com>
2024-07-04 09:50:52 -0300

    TIRANDO A CAGADA DE THIAGO

--- php/header.php
+++ php/header.php
@@ -5,7 +5,7 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-border-hrg">
             <div class="container-fluid">
                 <a class="navbar-brand" href="http://10.1.1.31:80/centralservicos/">
-                    <img src="http://10.1.1.31:80/centralservicos/resources/img/central-servicos.png" alt="Central de Serviço" style="width: 160px">
+                    <img src="img/central-servicos.png" alt="Central de Serviço" style="width: 160px">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navBarCentral" aria-controls="navBarCentral" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
