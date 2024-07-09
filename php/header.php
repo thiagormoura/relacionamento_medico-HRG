ba575b8eb242ddf486ac3273309d49c0f0e01a93
PedroHenriqueCustodiodeLima <ph4261009@gmail.com>
2024-07-09 10:10:05 -0300

    ajustes

--- php/header.php
+++ php/header.php
@@ -4,20 +4,20 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-border-hrg">
         <div class="container-fluid">
-            <a class="navbar-brand" href="http://10.1.1.31:80/centralservicos/">
+            <a class="navbar-brand" href="http://10.1.1.31:80/centralservicos/" title="Central de Serviço">
                 <img src="img/central-servicos.png" alt="Central de Serviço" style="width: 160px">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navBarCentral" aria-controls="navBarCentral" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <button class="btn" title="Registrar">
-                <a class="nav-link" href="index.php">Registrar Atendimento</a>
+            <button class="btn">
+                <a class="nav-link" href="index.php" title="Registrar">Registrar Atendimento</a>
             </button>
-            <button class="btn" title="Histórico de Atendimento">
-                <a class="nav-link" href="historico.php">Histórico</a>
+            <button class="btn">
+                <a class="nav-link" href="historico.php" title="Histórico de Atendimento">Histórico</a>
             </button>
-            <button class="btn" title="Histórico de Profissionais">
-                <a class="nav-link" href="profissionais.php">Registro Profissionais</a>
+            <button class="btn">
+                <a class="nav-link" href="profissionais.php" title="Histórico de Profissionais">Registro Profissionais</a>
             </button>
             <div class="collapse navbar-collapse" id="navBarCentral">
             </div>
@@ -40,6 +40,7 @@
         </div>
     </div>
 </header>
+
 
    
 
