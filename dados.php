e7e303a8079457d1b7040879f0824b024c62ebb5
PedroHenriqueCustodiodeLima <ph4261009@gmail.com>
2024-07-23 10:50:00 -0300

    ajustes

--- dados.php
+++ dados.php
@@ -1,16 +1,12 @@
 <?php
-include("conexao.php"); // Inclui o arquivo de conexão com o banco de dados
-
+include("conexao.php");
 $dbhost = "localhost";
 $dbname = "relacionamentomedico";
 $dbuser = "root";
 $dbpass = "";
-
-// Função para obter os dados do atendimento e do profissional
 function dadosProf($id, $conn) {
     $dados = array();
-    $dados['assuntonome'] = array(); // Inicializa o array de assuntonomes
-
+    $dados['assuntonome'] = array(); 
     $sql = "SELECT a.data AS data_atendimento, a.situacao AS situacao_atendimento, a.assunto, a.descricao, a.acoes, a.veiculo_atendimento,
                    p.nome AS nome_profissional, p.data_nascimento AS data_nascimento_profissional, p.cpf, p.telefone AS telefone_profissional, p.telefone2, p.registro, p.email AS email_profissional, 
                    p.endereco, p.especialidades, p.orgao,
@@ -20,9 +16,7 @@ function dadosProf($id, $conn) {
             INNER JOIN atendimento_has_assunto rela ON a.id = rela.atendimento
             INNER JOIN assunto ass ON ass.id = rela.assunto
             WHERE a.id = $id";
-
     $result = mysqli_query($conn, $sql);
-
     if ($result === false) {
         echo "Erro na consulta: " . $conn->error;
     } elseif ($result->num_rows > 0) {
@@ -47,29 +41,21 @@ function dadosProf($id, $conn) {
                 $dados['data_nascimento_profissional'] = $row['data_nascimento_profissional'];
                 $firstRow = false;
             }
-            $dados['assuntonome'][] = $row['assuntonome']; // Adiciona o assunto ao array
+            $dados['assuntonome'][] = $row['assuntonome'];
         }
     } else {
         echo "<p>Nenhum dado encontrado para o ID informado.</p>";
     }
-
     return $dados;
 }
-
-// Estabelece a conexão com o banco de dados
 $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-
-// Verifica a conexão
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
-
 $id = isset($_GET['id']) ? intval($_GET['id']) : 0;
-
-// Chamada da função para obter os dados
 $dados = dadosProf($id, $conn);
 
-$conn->close(); // Fecha a conexão com o banco de dados
+$conn->close(); 
 ?>
 
 <!DOCTYPE html>
@@ -130,7 +116,6 @@ $conn->close(); // Fecha a conexão com o banco de dados
     $subTitle = "Registro de atendimento"; 
     include 'php/header.php'; 
     ?>
-
     <main class="container_fluid d-flex justify-content-center align-items-center">
         <div class="form-group col-8 mt-5">
             <form id="formulario_index" method="post" action="/seu_script_php.php">
@@ -206,14 +191,12 @@ $conn->close(); // Fecha a conexão com o banco de dados
                         <div readonly><?= isset($dados['veiculo_atendimento']) ? htmlspecialchars($dados['veiculo_atendimento']) : "" ?></div>
                     </div>
                 </div>
-
                 <div class="row">
                     <label class="fs-4" for="">Assuntos Selecionados:</label>
                     <?php foreach ($dados['assuntonome'] as $assunto): ?>
                         <input class="col-12 form-control p-3" value="<?= htmlspecialchars($assunto) ?>" type="text" disabled>
                     <?php endforeach; ?>
                 </div>
-
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css" />
                 <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" />
                 <div class="container"> 
--- historico.php
+++ historico.php
@@ -55,7 +55,16 @@ $totalPaginas = ceil($totalRegistros / $registrosPorPagina);
     <link rel="stylesheet" href="css/selectize.bootstrap5.min.css">
     <link rel="stylesheet" href="css/multi-select-tag.css">
 </head>
-<style>
+<style>   
+    .swal2-confirm.btn-confirm {
+        background-color: #0B700B; 
+        border-color: #0B700B;
+    }
+
+    .swal2-cancel.btn-cancel {
+        background-color: #6F0D0A; 
+        border-color: #6F0D0A;
+    }
     .pagination {
             display: flex;
             justify-content: center;
@@ -86,13 +95,10 @@ $totalPaginas = ceil($totalRegistros / $registrosPorPagina);
     }
 </style>
 <body>
-    <!-- Parte do header e nav -->
     <?php
-
-    $pageTitle = "Histórico - Registros de Atendimento";
-    include 'php/header.php';
+        $pageTitle = "Histórico - Registros de Atendimento";
+        include 'php/header.php';
     ?>
-
 <main class="container-fluid d-flex justify-content-center align-items-center">
 <div class="form-group col-10 mt-5">
     <div class="accordion" id="accordionPanelsStayOpenExample">
@@ -151,10 +157,6 @@ $totalPaginas = ceil($totalRegistros / $registrosPorPagina);
             </div>
         </div>
     </div>
-
-
-
-
     <table class="table table-bordered table-striped mt-4">
         <thead class="thead-light align-middle">
         <tr>
@@ -164,8 +166,7 @@ $totalPaginas = ceil($totalRegistros / $registrosPorPagina);
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="35" height="35">
                         <path fill="#001f3f" d="M7 14l5-5 5 5H7z"/>
                     </svg>
-                </a>
-               
+                </a>           
             </th>
             <th class="text-left">Nome do Profissional</th>
             <th class="text-left">Assunto Tratado</th>
@@ -206,7 +207,6 @@ $totalPaginas = ceil($totalRegistros / $registrosPorPagina);
         const toggleOrderButton = document.getElementById('toggleOrder');
         let currentPage = 1;
         let ascending = true;
-
         function displayRows(data, startIndex, endIndex) {
     tableBody.innerHTML = '';
     for (let i = startIndex; i < endIndex; i++) {
@@ -217,6 +217,7 @@ $totalPaginas = ceil($totalRegistros / $registrosPorPagina);
         const assunto = row.assunto ? row.assunto : "Nenhum assunto encontrado";
         const situacao = row.situacao;
         const situacaoClass = situacao === 'Aberto' ? 'text-success' : 'text-danger';
+
         let buttonHTML = '';
         if (situacao === 'Aberto') {
             buttonHTML = `
@@ -230,13 +231,11 @@ $totalPaginas = ceil($totalRegistros / $registrosPorPagina);
             buttonHTML = `
                 <button id='finalizar-${row.id_atendimento}' class='btn btn-success' style='background-color: transparent; border: none;' title='Finalizado'>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width='20' height='20'>
-                        <!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                         <path fill='#03870C' d="M342.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 178.7l-57.4-57.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l80 80c12.5 12.5 32.8 12.5 45.3 0l160-160zm96 128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 402.7 54.6 297.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l256-256z"/>
                     </svg>
                 </button>
             `;
         }
-
         tableBody.innerHTML += `
             <tr>
                 <td class='text-left'>${dataAtendimentoFormatada}</td>
@@ -257,19 +256,18 @@ $totalPaginas = ceil($totalRegistros / $registrosPorPagina);
         `;
     }
 }
-
-
-
-
 function finalizeTask(id) {
     const button = document.getElementById(`finalizar-${id}`);
-
     Swal.fire({
         title: 'Você realmente deseja finalizar este atendimento?',
         icon: 'warning',
         showCancelButton: true,
         confirmButtonText: 'Sim',
-        cancelButtonText: 'Não'
+        cancelButtonText: 'Não',
+        customClass: {
+            confirmButton: 'btn-confirm',
+            cancelButton: 'btn-cancel'
+        }
     }).then((result) => {
         if (result.isConfirmed) {
             button.disabled = true;
@@ -296,7 +294,6 @@ function finalizeTask(id) {
 }
         function setupPagination(data) {
             pagination.innerHTML = '';
-
             const prevButton = document.createElement('button');
             prevButton.innerHTML = '&larr;';
             prevButton.disabled = currentPage === 1;
@@ -306,9 +303,7 @@ function finalizeTask(id) {
                 updatePagination(data);
             });
             pagination.appendChild(prevButton);
-
             const pageCount = Math.ceil(data.length / rowsPerPage);
-
             for (let i = 1; i <= pageCount; i++) {
                 const button = document.createElement('button');
                 button.textContent = i;
@@ -322,7 +317,6 @@ function finalizeTask(id) {
                 });
                 pagination.appendChild(button);
             }
-
             const nextButton = document.createElement('button');
             nextButton.innerHTML = '&rarr;';
             nextButton.disabled = currentPage === pageCount;
@@ -333,18 +327,15 @@ function finalizeTask(id) {
             });
             pagination.appendChild(nextButton);
         }
-
         function updatePagination(data) {
             const start = (currentPage - 1) * rowsPerPage;
             const end = start + rowsPerPage;
             displayRows(data, start, end);
             setupPagination(data);
         }
-
         function redirectToDetails(idAtendimento) {
             window.location.href = `dados.php?id=${idAtendimento}`;
         }
-
         function getBadgeClass(situacao) {
             switch (situacao) {
                 case 'Aberto':
@@ -357,13 +348,11 @@ function finalizeTask(id) {
                     return 'badge badge-custom bg-secondary';
             }
         }
-
         function applyFilters() {
             filterData = document.getElementById('filterData').value.trim();
             filterNome = document.getElementById('filterNome').value.trim().toLowerCase();
             filterAssunto = document.getElementById('filterAssunto').value.trim().toLowerCase();
             filterStatus = document.getElementById('filterStatus').value.trim().toLowerCase();
-
             const filteredData = rows.filter(row => {
                 const dataMatches = filterData === '' || new Date(row.data).toLocaleDateString('pt-BR') === filterData;
                 const nomeMatches = row.nome.toLowerCase().includes(filterNome);
@@ -372,13 +361,10 @@ function finalizeTask(id) {
 
                 return dataMatches && nomeMatches && assuntoMatches && statusMatches;
             });
-
             currentPage = 1;
             updatePagination(filteredData);
         }
-
         document.getElementById('applyFilters').addEventListener('click', applyFilters);
-
         toggleOrderButton.addEventListener('click', function() {
             ascending = !ascending;
             const sortedData = rows.sort((rowA, rowB) => {
@@ -390,7 +376,6 @@ function finalizeTask(id) {
             updatePagination(sortedData);
             toggleSortIcon(ascending);
         });
-
         function toggleSortIcon(ascending) {
             const icon = toggleOrderButton.querySelector('svg path');
             if (ascending) {
@@ -399,7 +384,6 @@ function finalizeTask(id) {
                 icon.setAttribute('d', 'M7 10l5 5 5-5H7z');
             }
         }
-
         updatePagination(rows);
     </script>
 </div>   
@@ -408,7 +392,5 @@ function finalizeTask(id) {
 <br>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
-
-
 </body>
 </html>
